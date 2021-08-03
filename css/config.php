acae1f4275babd39886e33a1f8f71e4867cb6a6e
Nahid Hasan <nahidjc80@gmail.com>
2021-08-03 11:02:07 +0600

    config added

---
+++ css/config.php
@@ -0,0 +1,14 @@
+<?php
+// connect to the database
+
+$databaseHost = 'localhost';
+$databaseName = 'crud_web_pca';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+// $db = mysqli_connect('localhost','root','','registration');
+
+
+
+?>
