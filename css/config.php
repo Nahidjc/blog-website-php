ad6e9bef2c58f73211e418c7257750afe30d4ccc
Nahid Hasan <nahidjc80@gmail.com>
2021-08-03 11:16:53 +0600

    register database created

--- css/config.php
+++ css/config.php
@@ -5,7 +5,7 @@ $databaseHost = 'localhost';
 $databaseName = 'crud_web_pca';
 $databaseUsername = 'root';
 $databasePassword = '';
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+$db = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
 // $db = mysqli_connect('localhost','root','','registration');
 
--- registration.php
+++ registration.php
@@ -11,14 +11,43 @@
     <title>register user</title>
 
     <?php
-
+    include_once("config.php");
 
 		$username = "";
 		$email ="";
 		$errors = array();
 
 		if(isset($_POST['registration'])){
-			$username = mysql_real_escape_string(($_POST['username']));
+      $username = $_POST['username'];
+      $email =  $_POST['email'];
+      $password_1 =  $_POST['password_1'];
+      $password_2 =  $_POST['password_2'];
+
+      if(empty($username)){
+        array_push($errors,"Username is required");
+      }
+      
+      if(empty($email)){
+        array_push($errors,"Email is required");
+      } 
+      
+      if(empty($password_1)){
+        array_push($errors,"Password is required");
+      }
+
+      if($password_1 != $password_2){
+        array_push($errors,"The two passwords do not match");
+      }
+
+      if(count($errors)==0){
+        $password = md5($password_1);
+        $sql = "INSERT INTO users (username,email,password_1)
+        VALUES ('$username','$email','$password')
+        ";
+
+        mysqli_query($db,$sql);
+      }
+
 		}
 
 
