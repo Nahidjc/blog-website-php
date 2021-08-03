9281969dc996b4ddbbfbdf4e985a112f434ecb54
Nahid Hasan <nahidjc80@gmail.com>
2021-08-03 11:39:21 +0600

    connected database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // connect to the database
 
 $databaseHost = 'localhost';
-$databaseName = 'crud_web_pca';
+$databaseName = 'blog-website-db';
 $databaseUsername = 'root';
 $databasePassword = '';
 $db = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
